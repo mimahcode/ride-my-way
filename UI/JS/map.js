01c490688b06f1f19d1273756029b992ba8f4706
unknown <semahalc@gmail.com>
2019-05-18 13:29:24 +0300

    #4 Added the landing page

--- UI/JS/map.js
+++ UI/JS/map.js
@@ -10,11 +10,3 @@ function showSideBar(){
 toggleBtn.addEventListener("click", () => {
     showSideBar()
 })
-
-//MAP SCRIPTS
-var mymap = L.map('mapid').setView([-6.8, 39.283333], 13);
-
-L.tileLayer('http://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; OpenStreetMap contributors'
-}).addTo(mymap);
-
